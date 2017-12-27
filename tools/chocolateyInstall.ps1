45a6b22e71657f5faae120f158e62b8ad03f059f
Ken Dale <ken@kendaleiv.com>
2017-12-27 08:24:43 -0500

    [WIP] 0.24.1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,12 +1,8 @@
 ﻿$packageName = "sql-operations-studio"
-$url = "https://go.microsoft.com/fwlink/?linkid=862648"
-$toolsDir = Get-ToolsLocation
-$checksum = "4D3E50DC59EC863418B5C8C0647C96A9B13AB92C817B45DD0CD8405DB690471F"
+$url = "https://go.microsoft.com/fwlink/?linkid=865305"
+$fileType = "exe"
+$silentArgs = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+$checksum = "BBD79D4E5C46FE439ED2FA9C74782ECA08B38290B66DC7DAA57C6E558B9295F5"
 $checksumType = "sha256"
 
-Install-ChocolateyZipPackage -PackageName $packageName -Url $url -UnzipLocation $toolsDir -Checksum $checksum -ChecksumType $checksumType
-
-$shortcutPath = Join-Path $([Environment]::GetFolderPath([System.Environment+SpecialFolder]::CommonPrograms)) 'SQL Operations Studio.lnk'
-$targetPath = Join-Path -Path $toolsDir "sqlops-windows" | Join-Path -ChildPath "sqlops.exe"
-
-Install-ChocolateyShortcut -ShortcutFilePath $shortcutPath -TargetPath $targetPath
+Install-ChocolateyPackage -PackageName $packageName -FileType $fileType -SilentArgs $silentArgs -Url $url -Checksum $checksum -ChecksumType $checksumType
