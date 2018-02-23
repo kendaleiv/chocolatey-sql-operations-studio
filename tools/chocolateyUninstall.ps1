afab898fa41aa0c51728fd783a43ee45e29b9b60
Ken Dale <ken@kendaleiv.com>
2018-02-22 19:55:43 -0500

    0.26.7

--- tools/chocolateyUninstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$toolsDir = Get-ToolsLocation
-$dir = Join-Path $toolsDir "sqlops-windows"
-
-Remove-Item $dir -Recurse
-
-$shortcutPath = Join-Path $([Environment]::GetFolderPath([System.Environment+SpecialFolder]::CommonPrograms)) 'SQL Operations Studio.lnk'
-If (Test-Path $shortcutPath) {
-  Remove-Item $shortcutPath
-}
