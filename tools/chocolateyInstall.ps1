b7f42d6d5d25a199a232d98c524ab83b95c5e39b
Ken Dale <ken@kendaleiv.com>
2018-05-22 17:04:12 -0400

    0.29.3

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -9,11 +9,11 @@ Write-Host "Merge Tasks: `n$mergeTasks"
 $packageArgs = @{
   packageName    = 'sql-operations-studio'
   fileType       = 'EXE'
-  url64bit       = 'https://go.microsoft.com/fwlink/?linkid=872717'
+  url64bit       = 'https://go.microsoft.com/fwlink/?linkid=873386'
 
   softwareName   = 'SQL Operations Studio (preview)'
 
-  checksum64     = 'D4D5CB0F9C3EA9AF117A5DD49C6078FE3CD348768B641D81BD5CA55BFA8B5A40'
+  checksum64     = '5E832491FDAFAD7FC279A214A3D13893202301D8D64505AB40CBE98D0DEDAC96'
   checksumType64 = 'sha256'
 
   silentArgs     = "/verysilent /suppressmsgboxes /mergetasks=""$mergeTasks"" /log=""$env:temp\sql-operations-studio.log"""
